aac6c698964e4520e6aab26d3998f4396ffd9183
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-03-28 01:24:34 +0530

    tute 2 done

---
+++ Tute 2/Tute_2/src/main/kotlin/Activity/try.kt
@@ -0,0 +1,12 @@
+package Activity
+
+val names = arrayOf("Jane","Ted","Alice","John",1,2,3,4,34.223,3L,23.1f)
+val numbers = intArrayOf(3,4,3,2,1,5,6,7,3)
+fun main() {
+    for (i in numbers){
+        println(i)
+    }
+    for(i in names){
+        println(i)
+    }
+}
