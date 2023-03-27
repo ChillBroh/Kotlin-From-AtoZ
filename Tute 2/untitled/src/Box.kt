55372eb1a371a8baf24786f1035e0fe36ed6d022
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-03-27 23:26:57 +0530

    added a class

---
+++ Tute 2/untitled/src/Box.kt
@@ -0,0 +1,17 @@
+
+class Box{
+    //properties
+    val length: Int = 10;
+    val width: Int = 20;
+    val height: Int = 5;
+
+    //behaviours
+    fun fillContents(){
+        print("Box is filled")
+    }
+
+    fun ope(){
+        print("Box is Open")
+    }
+
+}
