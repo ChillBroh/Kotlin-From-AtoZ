21c3ff2327ad939b59301452cb1a6c7fcc55c0c4
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-05-22 12:42:43 +0530

    calculator done

--- Tute 3/Calculator/app/src/main/java/com/example/calculator/DisplayActivity.kt
+++ Tute 3/Calculator/app/src/main/java/com/example/calculator/DisplayActivity.kt
@@ -7,13 +7,17 @@ import android.widget.Button
 import android.widget.TextView
 
 class DisplayActivity : AppCompatActivity() {
+    private lateinit var btnBack:Button;
+    private lateinit var txtAns:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        var txtAnswer = findViewById<TextView>(R.id.txtAnswer)
-        txtAnswer.setText(intent.getDoubleExtra("answer", 0.0).toString())
-        var btnBack = findViewById<Button>(R.id.btnBack)
+        btnBack = findViewById(R.id.btnBack)
+        txtAns = findViewById(R.id.TxtAnswerShow)
+
+        txtAns.text = intent.getDoubleExtra("answer", 0.0).toString()
+
         btnBack.setOnClickListener {
             var intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
